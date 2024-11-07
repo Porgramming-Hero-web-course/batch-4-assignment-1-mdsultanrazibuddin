ea20305573958eddafb560af22ce8ca76124fbb3
mdsultanrazibuddin <mdsultanrazibuddin@gmail.com>
2024-11-07 21:14:47 +0600

    added

--- src/problem-2.ts
+++ src/problem-2.ts
@@ -7,6 +7,6 @@
         return remove;
     }
 
-    const newArray = removeDuplicates([1, 2, 2, 3, 4, 4, 5])
-    console.log(newArray);
+    // const newArray = removeDuplicates([1, 2, 2, 3, 4, 4, 5])
+    // console.log(newArray);
     
